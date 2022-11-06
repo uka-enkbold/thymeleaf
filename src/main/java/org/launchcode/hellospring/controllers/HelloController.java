6adc5216e28dad1d669fed85383ab71cd2a1ee40
uka-enkbold <uuganchimeg1104@gmail.com>
2022-11-06 08:44:38 -0600

    dynamic thymeleaf template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,37 +5,31 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
-        @GetMapping("hello")
+    // Responds to /hello?name=LaunchCode
+    @RequestMapping(value = "hello", method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
-    public String hello (@PathVariable String name){
-
-            return "Hello, " + name + "!";
+    public String hello(@RequestParam String name) {
+        return "Hello, II + name";
     }
-    @GetMapping("hello/{name}")
+    // Responds to /hello/LaunchCode
+    @GetMapping( "hello/{name}")
     @ResponseBody
-    public String helloWithQueryParameter(@PathVariable String name) {
-        return "Hello, " + name + "!";
-
+    public String helloAgain(@PathVariable String name) {
+        return "Hello, в + name +";
     }
-    @GetMapping("form")
-    @ResponseBody
+    @GetMapping( "form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = 'hello'>" +
-                "<input type = 'text' name = 'name'>" +
-                "<input type = 'submit' value='Greet me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
-    }
-
-    @PostMapping("goodbye")
-    @ResponseBody
-    public String goodbye() {
-        return "Good BYE!";
+        return "form";
     }
 }
+
+
+//    @PostMapping("goodbye")
+//    @ResponseBody
+//    public String goodbye() {
+//        return "Good BYE!";
+//    }
+//}
 //    @RequestMapping(value = "helloGoodbye", method = {RequestMethod.GET, RequestMethod.POST})
 //    public String helloGoodBye(){
 //        return "Hello, Goodbye";
